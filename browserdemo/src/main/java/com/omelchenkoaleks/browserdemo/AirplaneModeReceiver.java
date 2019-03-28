6ae5499ce1b4e7359f1a3a0807ea699dfa0e53a5
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-28 08:08:27 +0200

    весь код отслеживаемого события в одном классе Receiver ...

--- browserdemo/src/main/java/com/omelchenkoaleks/browserdemo/AirplaneModeReceiver.java
+++ browserdemo/src/main/java/com/omelchenkoaleks/browserdemo/AirplaneModeReceiver.java
@@ -1,13 +1,34 @@
 package com.omelchenkoaleks.browserdemo;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class AirplaneModeReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (intent != null && !intent.getBooleanExtra("state", false))
         Log.d("happy", "onReceive");
+
+        Intent google = new Intent(Intent.ACTION_VIEW);
+        Uri uri = Uri.parse("http://google.com");
+        google.setData(uri);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context, 1, google, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentTitle("google")
+                .setContentText("to Google: ...")
+                .setSmallIcon(android.R.drawable.btn_radio)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat.from(context).notify(1, builder.build());
     }
 }
