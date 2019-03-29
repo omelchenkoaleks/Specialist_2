0c80f41a2dab596373a7cb8b8b8a98364cf5e191
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-29 06:53:14 +0200

    первый способ добавления фрагмента в пользовательский интерфейс - в layout Активити добавить его как элемент и зарегистрировать name в этой же разметке ...

--- fragments/src/main/java/com/omelchenkoaleks/fragments/Logger.java
+++ fragments/src/main/java/com/omelchenkoaleks/fragments/Logger.java
@@ -9,17 +9,17 @@ public class Logger {
     public static void logMe() {
         StackTraceElement[] stack = Thread.currentThread().getStackTrace();
         // выводим весь стек
-        for (int i = 0; i < stack.length; i++) {
-            Log.d("happy", ""
-                    + i
-                    + " "
-                    + stack[i].getClassName()
-                    + " "
-                    + stack[i].getMethodName());
-        }
+//        for (int i = 0; i < stack.length; i++) {
+//            Log.d("happy", ""
+//                    + i
+//                    + " "
+//                    + stack[i].getClassName()
+//                    + " "
+//                    + stack[i].getMethodName());
+//        }
 
         // в стеке подсмотрели номер элемента массива нужный и теперь его только можем вывести ...
-            Log.d("happy", stack[3].getClassName() + " " + stack[3].getMethodName());
-        }
+        Log.d("happy", stack[3].getClassName() + " " + stack[3].getMethodName());
     }
+
 }
--- fragments/src/main/java/com/omelchenkoaleks/fragments/MainActivity.java
+++ fragments/src/main/java/com/omelchenkoaleks/fragments/MainActivity.java
@@ -10,7 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Logger.logMe();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Logger.logMe();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Logger.logMe();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Logger.logMe();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Logger.logMe();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
         Logger.logMe();
     }
 }
