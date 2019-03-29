a92e8d5d6c436e9ba03939fa51b048852450eb86
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-29 08:03:28 +0200

    добавляем фрагмент в пользовательский интерфейс - в коде активности, используя FragmentManager ...

--- fragments/src/main/java/com/omelchenkoaleks/fragments/MainActivity.java
+++ fragments/src/main/java/com/omelchenkoaleks/fragments/MainActivity.java
@@ -1,16 +1,32 @@
 package com.omelchenkoaleks.fragments;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameLayout mNestFrameLayout;
+    private FragmentTransaction mFragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Logger.logMe();
         setContentView(R.layout.activity_main);
+
+        mNestFrameLayout = findViewById(R.id.nest_fl);
+
+        // получаем менеджер
+        FragmentManager manager = getSupportFragmentManager();
+        // начинаем транзакцию
+        mFragmentTransaction = manager.beginTransaction();
+        // между началом транзакции и завершение транзакции выполняем какие-то действия
+        mFragmentTransaction.add(R.id.nest_fl, new MyFragment());
+        // завершаем транзакцию
+        mFragmentTransaction.commit();
     }
 
     @Override
