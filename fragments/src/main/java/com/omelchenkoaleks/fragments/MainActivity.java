335243a904b0d88a7379b0e82f15d1fd0e2d2324
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-29 07:47:11 +0200

    от одного класса в разметке создаем второй экземпляр фрагмета и выводим его в UI ...

--- fragments/src/main/java/com/omelchenkoaleks/fragments/MainActivity.java
+++ fragments/src/main/java/com/omelchenkoaleks/fragments/MainActivity.java
@@ -9,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Logger.logMe();
+        setContentView(R.layout.activity_main);
     }
 
     @Override
