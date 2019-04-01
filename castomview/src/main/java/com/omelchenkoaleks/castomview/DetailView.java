37e939f102b02effc941f87e7f83ed7db1246caa
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-04-01 17:50:14 +0300

    пример того, как поделить на две части - в одной список, в другой текст - кастомное вью ... добавил библиотеку Percent Support Library ...

---
+++ castomview/src/main/java/com/omelchenkoaleks/castomview/DetailView.java
@@ -0,0 +1,13 @@
+package com.omelchenkoaleks.castomview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class DetailView extends LinearLayout {
+    public DetailView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
