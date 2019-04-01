14de77db35c78df729ab7ca9689cd185159e97dc
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-04-01 18:04:46 +0300

    памятка: при работе с кастомными вью метод findViewById живет в методе onFinishInflate ...

--- castomview/src/main/java/com/omelchenkoaleks/castomview/DetailView.java
+++ castomview/src/main/java/com/omelchenkoaleks/castomview/DetailView.java
@@ -3,11 +3,26 @@ package com.omelchenkoaleks.castomview;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class DetailView extends LinearLayout {
+    TextView itemView;
+
     public DetailView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    // при работе с кастомными вью findViewById находится в этом методе ...
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        itemView = findViewById(R.id.info_tv);
+    }
+
+    public void showItem(String item) {
+        itemView.setText(item);
     }
 }
