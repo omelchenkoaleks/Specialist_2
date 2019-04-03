b6a2ca1f6244db29e1b109662a88bc181080b6b6
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-04-03 09:13:52 +0300

    запускаем поток thread с методом sleep ...

--- javalib/src/main/java/com/omelchenkoaleks/javalib/MyClass.java
+++ javalib/src/main/java/com/omelchenkoaleks/javalib/MyClass.java
@@ -3,5 +3,20 @@ package com.omelchenkoaleks.javalib;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        MyThread t1 = new MyThread();
+        t1.start();
+    }
+
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                sleep(20000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("thread");
+        }
     }
 }
