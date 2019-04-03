d30aa1c83b17958e72989473ed1f93ff930b1fbf
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-04-03 09:27:24 +0300

    метод interrupt - и обработка try / catch ...

--- javalib/src/main/java/com/omelchenkoaleks/javalib/MyClass.java
+++ javalib/src/main/java/com/omelchenkoaleks/javalib/MyClass.java
@@ -6,17 +6,28 @@ public class MyClass {
 
         MyThread t1 = new MyThread();
         t1.start();
+
+        System.out.println("wow!");
+
+        /*
+        методо посылвет сообщение thread, что пора завершаться ...
+         */
+//        t1.interrupt();
     }
 
+    /*
+    нужен, чтобы запустить какую-то отдельную задачу (в отдельном потоке) ...
+     */
     public static class MyThread extends Thread {
         @Override
         public void run() {
             try {
                 sleep(20000);
+                System.out.println("thread");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("thread");
+
         }
     }
 }
