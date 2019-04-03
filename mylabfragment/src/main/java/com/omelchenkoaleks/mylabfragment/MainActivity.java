a5e73c4da554b3c5a13b154689564394b31afad9
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-04-03 10:39:18 +0300

    ПРИМЕР: падение приложения по причиние попытки изменить что-то в главном потоке приложения из другого потока ... ЗАПРЕЩЕНО ...

--- mylabfragment/src/main/java/com/omelchenkoaleks/mylabfragment/MainActivity.java
+++ mylabfragment/src/main/java/com/omelchenkoaleks/mylabfragment/MainActivity.java
@@ -2,21 +2,38 @@ package com.omelchenkoaleks.mylabfragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button mButton_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton_2 = findViewById(R.id.button_2_bt);
     }
 
     public void doSomethingLong() {
         try {
             System.out.println("long operation starts");
+
+            /**
+             * Многопоточность в пользовательском интерфейсе (Андроид) - запрещена!!!
+             *
+             * поэтому, невозможно изменить название кнопки из другого потока в UI - потоке ...
+             */
+
             Thread.sleep(5000); // это любая долгая операция
+
+            /*
+                 ЗДЕСЬ УПАДЕТ ПРИЛОЖЕНИЕ ...
+             */
+            mButton_2.setText("done");
+
             System.out.println("long operation stops");
         } catch (InterruptedException e) {
             e.printStackTrace();
