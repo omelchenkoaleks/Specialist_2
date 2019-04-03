8fcd9976b11270e8ee099219aad07f52dd90b64e
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-04-03 10:55:26 +0300

    Handler позволяет поставить в очередь UI-потока задачу для выполнения ...

--- mylabfragment/src/main/java/com/omelchenkoaleks/mylabfragment/MainActivity.java
+++ mylabfragment/src/main/java/com/omelchenkoaleks/mylabfragment/MainActivity.java
@@ -1,12 +1,14 @@
 package com.omelchenkoaleks.mylabfragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Handler mHandler;
     Button mButton_2;
 
     @Override
@@ -15,23 +17,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton_2 = findViewById(R.id.button_2_bt);
+
+        /*
+        с помощью Handler мы поставили в очередь UI-потока еще одну задачу и Looper выполнил ее
+         */
+        mHandler = new Handler();
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("I`m in Runnable!");
+            }
+        });
     }
 
     public void doSomethingLong() {
         try {
             System.out.println("long operation starts");
 
-            /**
-             * Многопоточность в пользовательском интерфейсе (Андроид) - запрещена!!!
-             *
-             * поэтому, невозможно изменить название кнопки из другого потока в UI - потоке ...
-             */
-
             Thread.sleep(5000); // это любая долгая операция
-
-            /*
-                 ЗДЕСЬ УПАДЕТ ПРИЛОЖЕНИЕ ...
-             */
             mButton_2.setText("done");
 
             System.out.println("long operation stops");
@@ -46,12 +49,6 @@ public class MainActivity extends AppCompatActivity {
         t1.start();
     }
 
-    /*
-    теперь вместо того, чтобы запускать долгую операцию из обработчика событий,
-    мы ее выселили непосредственно в отдельный поток Thread ...
-
-    ТЕПЕРЬ: главный поток приложения освобожден от долгой операции ...
-     */
     public class WorkerThread extends Thread {
         @Override
         public void run() {
