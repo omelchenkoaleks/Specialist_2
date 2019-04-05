2eb8f23f73805cea4b0a599e5c584357e3a59ed8
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-04-05 15:54:15 +0300

    добавили уведомление (появится в заданное время) - при нажатии на которое откроется наше активити ...

--- alarmmanager/src/main/java/com/omelchenkoaleks/alarmmanager/EventService.java
+++ alarmmanager/src/main/java/com/omelchenkoaleks/alarmmanager/EventService.java
@@ -1,11 +1,14 @@
 package com.omelchenkoaleks.alarmmanager;
 
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class EventService extends IntentService {
     public static final String NUM_E = "num";
@@ -20,8 +23,25 @@ public class EventService extends IntentService {
         super("EventService");
     }
 
+    // все, что в этом методе происходит в (background thread) фоновом потоке ...
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Log.d("happy", "onHandleIntent " + intent.getIntExtra(NUM_E, -1));
+        int num = intent.getIntExtra(NUM_E, -1);
+        Log.d("happy", "onHandleIntent " + num);
+
+        Intent uaIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 2, uaIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // используем для пробуждения - уведомление ...
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                        .setContentTitle("heloo")
+                        .setContentText("got " + num)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true); // флажок означает, что при нажатии на уведомление - оно исчезнет ...
+
+        NotificationManagerCompat.from(this).notify(1, builder.build());
     }
 }
